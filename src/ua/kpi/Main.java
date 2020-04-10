84ac6026699b9cb530cc89563a60e771d51b5fd7
deyad <deyadecember@gmail.com>
2020-04-10 12:49:15 +0300

    initial commit( simple Kruskal's algorithm)

---
+++ src/ua/kpi/Main.java
@@ -0,0 +1,19 @@
+package ua.kpi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int vertices = 6;
+        Graph graph = new Graph(vertices);
+        graph.addEgde(0, 1, 4);
+        graph.addEgde(0, 2, 3);
+        graph.addEgde(1, 2, 1);
+        graph.addEgde(1, 3, 2);
+        graph.addEgde(2, 3, 4);
+        graph.addEgde(3, 4, 2);
+        graph.addEgde(4, 5, 6);
+        graph.kruskalMST();
+    }
+
+}
